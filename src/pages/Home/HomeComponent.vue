6d7cde476801f18627375793fe2ec7e555391fa7
Daniel Rezende Lima <daniel-rl-98@hotmail.com>
2022-08-14 19:19:10 -0300

    19:19

--- src/pages/Home/HomeComponent.vue
+++ src/pages/Home/HomeComponent.vue
@@ -1,14 +1,16 @@
 <template>
     <section class="home">
-        <div class="container">
+        <main class="container">
             <div class="grid-one">       
             </div>
             <div class="grid-two">
                 <div class="title">
-                    <h1>Criação de sites personalizados para seu négocio</h1>
+                    <h1>Criação de site personalizado para seu négocio</h1>
+                    <p>Desenvolvimento de site com um design moderno, atrativo, responsivo e pronto para indexar no google.</p>
+                    <a href="https://google.com">Entrar em contato</a>
                 </div>
             </div>
-        </div>
+        </main>
     </section>
 </template>
 <script>
